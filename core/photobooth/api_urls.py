8ca00dbbe2c51443c41a91d7d4265cd08039eaf9
doloi <doloiueh@gmail.com>
2026-04-07 11:52:04 +0700

    fix code

--- core/photobooth/api_urls.py
+++ core/photobooth/api_urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from core.photobooth.api.capture_options_view import CaptureOptionsView
 from core.photobooth.api.capture_package_list_view import CapturePackageListView
 from core.photobooth.api.device_register_view import PhotoboothDeviceRegisterView
+from core.photobooth.api.order_gallery_view import OrderGalleryView
 from core.photobooth.api.remove_background_view import RemoveBackgroundView
 from core.photobooth.api.save_images_view import SaveImagesView
 
@@ -12,4 +13,5 @@ urlpatterns = [
     path('capture-options/', CaptureOptionsView.as_view(), name='photobooth-capture-options'),
     path('remove-background/', RemoveBackgroundView.as_view(), name='photobooth-remove-background'),
     path('save-images/', SaveImagesView.as_view(), name='photobooth-save-images'),
+    path('gallery/<str:order_id>/', OrderGalleryView.as_view(), name='photobooth-order-gallery'),
 ]
